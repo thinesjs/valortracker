0e232b29fb05cd64c4a831f9a8bb8f770ad30f3f
thinesjs <thinesjs@gmail.com>
2023-02-13 18:32:18 +0800

    footer update

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer">{{ config('app.name', 'ValorTracker - Login') }} was created under Riot Games' "Legal Jibber Jabber" policy using assets owned by Riot Games.  Riot Games does not endorse or sponsor this project.</footer>
+<footer class="footer text-muted">{{ config('app.name', 'ValorTracker - Login') }} was created under Riot Games' "Legal Jibber Jabber" policy using assets owned by Riot Games.  Riot Games does not endorse or sponsor this project.</footer>
 </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
