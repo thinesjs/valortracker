4f357e57f068d1ceff5bdc9c487f2a32eaf06a96
thinesjs <thinesjs@gmail.com>
2023-02-15 19:50:29 +0800

    update

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'ValorTracker') }}</title>
+    <meta name="robots" content="noindex,nofollow">
+    @vite('resources/js/app.js')
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+</head>
+<body>
+    <div class="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1 class="error-title text-danger">401</h1>
+                <h3 class="text-uppercase error-subtitle">UNAUTHORIZED</h3>
+                <p class="text-muted m-t-30 m-b-30">Looks like you're not allowed here! Fear not, we can navigate you out!</p>
+                <div class="mb-3">
+                    <a href="" class="btn btn-info btn-rounded waves-effect waves-light m-b-40 text-white shadow"><i class="ri-restart-line me-2"></i>Refresh Page</a>
+                </div>
+                <div class="mb-3">
+                    <a href="{{route('/')}}" class="btn btn-danger btn-rounded waves-effect waves-light m-b-40 text-white shadow"><i class="ri-home-3-line me-2"></i>Home</a>
+                </div>
+                
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- All Required js -->
+    <!-- ============================================================== -->
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    {{-- <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <!-- ============================================================== -->
+    <!-- This page plugin js -->
+    <!-- ============================================================== -->
+    <script>
+        $(".preloader").fadeOut();
+    </script>
+    @vite('resources/dist/js/waves.js')
+</body>
+</html>
