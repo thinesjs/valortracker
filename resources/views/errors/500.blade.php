5dec47ac64237473affccaa54b5b9a2fb644cbff
thinesjs <thinesjs@gmail.com>
2023-02-15 19:42:10 +0800

    update

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -31,7 +31,7 @@
             <div class="error-body text-center">
                 <h1 class="error-title text-danger">500</h1>
                 <h3 class="text-uppercase error-subtitle">SERVER ERROR</h3>
-                <p class="text-muted m-t-30 m-b-30"></p>
+                <p class="text-muted m-t-30 m-b-30">Looks like you're navigating too fast! Give it some break and try again!</p>
                 <a href="{{route('/')}}" class="btn btn-danger btn-rounded waves-effect waves-light m-b-40 text-white shadow">Back to home</a>
             </div>
         </div>
